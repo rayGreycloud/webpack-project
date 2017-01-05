831a6b6ec2f34c88a1324ffd786164b3ec20fdad
rayGreycloud <rayGreycloud@gmail.com>
2017-01-05 11:40:00 -0800

    Add code-splittin optimization to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,22 @@
 var webpack = require('webpack');
 var path = require('path');
+// Specify vendor dependencies
+const VENDOR_LIBS = [
+  'faker', 'lodash', 'react', 'react-dom',
+  'react-input-range', 'react-redux', 'react-router',
+  'redux', 'redux-form', 'redux-thunk'
+];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    // Specify second bundle file
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    // Use name from entry
+    filename: '[name].js'
   },
   module: {
     rules: [
@@ -19,5 +30,11 @@ module.exports = {
         test: /\.css$/
       }
     ]
-  }
+  },
+  plugins: [
+    // Eliminate duplicate code in bundles
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    })
+  ]
 };
